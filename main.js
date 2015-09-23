0a4fcf7eff79914713e20490f250542e9d2453af
thisismyusername2015 <thisismyusername2015@users.noreply.github.com>
2015-09-23 15:58:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var DEBUG = false;
 var SPEED = 430; //Speed
-var GRAVITY = 77; //Vertical Speed
+var GRAVITY = 57; //Vertical Speed
 var FLAP = 470;
 var SPAWN_RATE = 1 / 1.2;
 var OPENING = 600; //Space prevents you from collusion 
